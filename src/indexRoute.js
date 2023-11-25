3c8c18f34a56edce10b2d7cd02f89ccd2c276241
aditya chauhan <ankitchmca@gmail.com>
2023-11-25 13:34:58 +0530

    reeduce packages

--- src/indexRoute.js
+++ src/indexRoute.js
@@ -2,10 +2,8 @@ const express = require('express')
 const router = express.Router()
 router.use('/user', require('./routes/userRoute'))
 router.use('/bhws', require('./routes/bhwsRoute'))
-router.use('/anime', require('./routes/AnimeRoute'))
+router.use('/anime', require('./routes/animeRoute'))
 router.use('/scripts', require('./routes/projectScripts'))
 router.use('/app', require('./routes/appStatics'))
-
-
 
 module.exports = router
