58d90e5596c9be0abd9eed52e33f8b120e04955c
aditya chauhan <aditya.chauhan@quytech.com>
2023-12-03 16:38:27 +0530

    db changes

--- connections/db.js
+++ connections/db.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 // mongodb://vwatch.in:27017/vwatch
 // mongodb://127.0.0.1:27017/vwatch
 mongoose
-  .connect('mongodb://vwatch.in:27017/vwatch')
+  .connect(
+    'mongodb+srv://vwatchT1:lifehope@serverlessvwatcht1.s1ak6wr.mongodb.net/'
+  )
   .then(() => {
     console.log('Connection is set.')
   })
