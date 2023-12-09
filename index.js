4eb42a58c089e8f2a81606c2ec4f9b4b5d5c667c
aditya chauhan <aditya.chauhan@quytech.com>
2023-12-09 16:29:48 +0530

    ad

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser')
 const { errors } = require('celebrate')
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json')
-const socketIO = require('socket.io')
 
 const fs = require('fs')
 
@@ -35,14 +34,6 @@ app.use(errors())
 app.get('/', async (req, res) => {
   return res.json({ status: true, message: 'Welcome to server' })
 })
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log('app listening on port:', port)
-})
-const io = socketIO(server)
-
-io.on('connection', (socket) => {
-  console.log('User connected:')
-  socket.on('disconnect', () => {
-    console.log('User disconnected')
-  })
 })
